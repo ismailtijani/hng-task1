b86ff7604913f93da4d09a9a7ff27237f3a8ff02
son-of-tijani <ismailtijani10@yahoo.com>
2022-10-31 15:32:39 +0100

    Passing data in Json

--- app.js
+++ app.js
@@ -3,11 +3,8 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
