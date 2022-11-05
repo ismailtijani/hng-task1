58517b197be4819cb32e01d598c15ec299974eec
son-of-tijani <ismailtijani10@yahoo.com>
2022-11-05 22:12:14 +0100

    Second Task

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-// res.setHeader("Content-Type", "application/json");
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
 
@@ -20,6 +19,72 @@ app.get("/", (req, res) => {
   });
 
   res.send(data);
+});
+
+const OperationType = Object.freeze({
+  ADD: "add",
+  SUBTRACT: "subtract",
+  MULTIPLY: "multiply",
+  PLUS: "plus",
+  MINUS: "minus",
+  TIMES: "times",
+});
+
+app.post("/", (req, res) => {
+  const { operation_type } = req.body;
+
+  if (!operation_type) {
+    return res.status(400).json({
+      error: "Operation type is required",
+    });
+  }
+
+  let result = 0;
+
+  const extractDigits = operation_type.match(/\d+/g);
+
+  const x = parseInt(extractDigits[0]);
+  const y = parseInt(extractDigits[1]);
+
+  if (!x || !y) {
+    return res.status(400).json({
+      error: "Missing required parameter",
+    });
+  }
+
+  const extractOperationType = operation_type.match(
+    /add|subtract|multiply|plus|minus|times/i
+  );
+  const convertedType = extractOperationType[0].toLowerCase();
+
+  if (!Object.values(OperationType).includes(convertedType)) {
+    return res.status(400).json({
+      error: "Invalid operation type",
+    });
+  }
+
+  switch (convertedType) {
+    case OperationType.ADD:
+    case OperationType.PLUS:
+      result = x + y;
+      break;
+    case OperationType.SUBTRACT:
+    case OperationType.MINUS:
+      result = x - y;
+      break;
+    case OperationType.MULTIPLY:
+    case OperationType.TIMES:
+      result = x * y;
+      break;
+    default:
+      break;
+  }
+
+  res.status(200).json({
+    slackUserName: "Ismail Tijani",
+    operation_type: convertedType,
+    result,
+  });
 });
 
 app.listen(port, () => console.log("Server is running on " + port));
